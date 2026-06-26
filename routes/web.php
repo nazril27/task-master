066ea1a85152d5224d3f8e5cff5fb12fa2b2fb53
nazril <dnazrilsanz@gmail.com>
2026-06-26 11:53:09 +0700

    buat routing crud
    - setelah melakukan desain database jangan lupa untuk diterapkan ke
      databasenya dengan perintah php artisan migrate
    - buka file routes/web.php dan buat routing untuk crud

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TugasController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function() {
+    return redirect()->route('tugas.index');
 });
+
+Route::resource('tugas', TugasController::class);
